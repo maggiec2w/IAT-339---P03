30698d32ead12528533e9d5ce8dd56ee0d269553
Maggie <magchanyw@gmail.com>
2019-03-21 20:38:08 -0700

    made changes to the homepage and also added two pages

---
+++ P03 - Portfolio/main.js
@@ -0,0 +1,11 @@
+
+var myVideo = document.getElementById("videographer-img"); 
+
+function vidPlay() {  
+    myVideo.play();  
+    // alert("working");
+} 
+function vidPause() {  
+    myVideo.pause();  
+    // alert("working");
+} 
