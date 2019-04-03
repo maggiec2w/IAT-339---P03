19c8736f0ffd26f6506563d49cf8f3616571c4c6
Maggie <magchanyw@gmail.com>
2019-04-03 16:15:13 -0700

    Edited again

--- P03 - Portfolio/main.js
+++ P03 - Portfolio/main.js
@@ -1,11 +1,22 @@
 
 var myVideo = document.getElementById("videographer-img"); 
+var myFoodieVideo = document.getElementById("foodie-img"); 
 
 function vidPlay() {  
     myVideo.play();  
-    // alert("working");
+  
 } 
 function vidPause() {  
     myVideo.pause();  
+    // alert("working");
+} 
+
+function vidFoodPlay() {  
+    myFoodieVideo.play();  
+  
+} 
+
+function vidFoodPause() {  
+    myFoodieVideo.pause();  
     // alert("working");
 } 
